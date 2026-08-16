52ed32b5f92586f067b2eb3a8ff35cdde454510d
Shaihueque <shaihueque@gmail.com>
2026-08-16 19:26:27 -0300

    Creamos el proyecto y la organizamos carpetas

---
+++ src/app.js
@@ -0,0 +1,5 @@
+export const app = {
+ name: 'Sistema Backend de Turnos y Reservas',
+ version: '1.0.0',
+ status: 'initial setup'
+ };
---
+++ src/server.js
@@ -0,0 +1,3 @@
+import { app } from './app.js';
+console.log('Aplicación inicializada');
+ console.log(app);

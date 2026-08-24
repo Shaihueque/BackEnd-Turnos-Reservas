17ca1eb19683a4ca631e6abcde87e9d9092b2b26
Shaihueque <shaihueque@gmail.com>
2026-08-24 17:38:07 -0300

    utilizamos dotenv

--- src/server.js
+++ src/server.js
@@ -1,3 +1,9 @@
 import { app } from './app.js';
+import { config } from './config/env.js';
+
+app.listen(config.port, () => {
+  console.log(`Servidor corriendo en modo ${config.nodeEnv} en el puerto ${config.port}`);
+});
+
 console.log('Aplicación inicializada');
  console.log(app);
